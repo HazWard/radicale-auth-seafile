55ffac4c4e1def0a30f803fee4588db71699ec77
Starly Solon <starlysolon@gmail.com>
2018-03-19 21:15:49 -0400

    Using Seafile API to authenticate

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ from setuptools import setup
 
 setup(
     name = "radicale_auth_seafile",
-    version = "0.1.2",
-    author = "Klemens Schölhorn",
-    author_email = "klemens@schoelhorn.eu",
-    description = (" Authenticate Radicale 2 requests against Seafile "),
+    version = "0.1.3",
+    author = "HazWard",
+    description = ("Authenticate Radicale 2 requests against Seafile"),
     license = "GPLv3",
     keywords = "radicale seafile auth",
-    url = "https://github.com/klemens/radicale-auth-seafile",
+    url = "https://github.com/hazward/radicale-auth-seafile",
     packages = ["radicale_auth_seafile"],
     install_requires = [
-        "passlib>=1.7.0",
-        "psycopg2",
+        "requests",
     ],
 )
